184e23cd37ffa7b36e4f22deaa2be527acf508d4
Veekthor01 <veekthor01@gmail.com>
2023-11-01 21:25:18 +0300

    footer component

---
+++ chronicles/src/app/Footer/page.js
@@ -0,0 +1,14 @@
+export default function Footer () {
+    return (
+        <footer className="bg-gray-800 py-4 mt-7">
+            <div className="container mx-auto px-4">
+                <p className="text-center text-gray-400 text-sm mb-3">
+                    &copy; 2023 Shortly. All rights reserved.
+                </p>
+                <p className="text-center text-gray-400 text-sm">
+                    Created by Veekthor
+                </p>
+            </div>
+        </footer>
+    );
+};
