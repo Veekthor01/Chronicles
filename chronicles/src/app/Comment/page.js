ea9c20aad5ea89e8ffaf7820224e75396eb8487e
Veekthor01 <veekthor01@gmail.com>
2023-11-02 23:34:37 +0300

    update comment section

--- chronicles/src/app/Comment/page.js
+++ chronicles/src/app/Comment/page.js
@@ -28,28 +28,32 @@ export default function NewComment({ blogPostId }) {
     };
     
     return (
-        <div>
-        <h1>Create a New Comment</h1>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="author">Author:</label>
-            <input
-            type="text"
-            id="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            className="text-black"
-            required
-            />
-            <label htmlFor="content">Content:</label>
-            <textarea
-            id="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            className="text-black"
-            required
-            />
-            <button type="submit">Submit</button>
-        </form>
-        </div>
+        <div className="max-w-md mx-auto p-4">
+  <h1 className="text-2xl font-bold mb-4">Create a New Comment</h1>
+  <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md">
+    <div className="mb-4">
+      <label htmlFor="author" className="text-gray-600">Author:</label>
+      <input
+        type="text"
+        id="author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+        className="w-full py-2 px-3 rounded-lg border border-gray-300 text-black focus:outline-none focus:border-indigo-500"
+        required
+      />
+    </div>
+    <div className="mb-4">
+      <label htmlFor="content" className="text-gray-600">Content:</label>
+      <textarea
+        id="content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        className="w-full py-2 px-3 rounded-lg border border-gray-300 text-black focus:outline-none focus:border-indigo-500"
+        required
+      />
+    </div>
+    <button type="submit" className="bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-700 transition duration-300">Submit</button>
+  </form>
+</div>
     );
     }
