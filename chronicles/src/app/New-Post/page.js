be55036a20d28b1653e5a7ff7ef2b024923a48c8
Veekthor01 <veekthor01@gmail.com>
2023-11-02 23:36:04 +0300

    update post page

--- chronicles/src/app/New-Post/page.js
+++ chronicles/src/app/New-Post/page.js
@@ -29,37 +29,44 @@ export default function NewBlogPost () {
   };
 
   return (
-    <div>
-      <h1>Create a New Blog Post</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title:</label>
+    <div className="max-w-md mx-auto p-4">
+    <h1 className="text-2xl font-bold text-center mb-4">Create a New Blog Post</h1>
+    <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md">
+      <div className="mb-4">
+        <label htmlFor="title" className="text-gray-600">Title:</label>
         <input
           type="text"
           id="title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          className="text-black"
+          className="w-full py-2 px-3 rounded-lg text-black border border-gray-300 focus:outline-none focus:border-indigo-500"
           required
         />
-        <label htmlFor="author">Author:</label>
+      </div>
+      <div className="mb-4">
+        <label htmlFor="author" className="text-gray-600">Author:</label>
         <input
           type="text"
           id="author"
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
-          className="text-black"
+          className="w-full py-2 px-3 rounded-lg border text-black border-gray-300 focus:outline-none focus:border-indigo-500"
           required
         />
-        <label htmlFor="content">Content:</label>
+      </div>
+      <div className="mb-4">
+        <label htmlFor="content" className="text-gray-600">Content:</label>
         <textarea
           id="content"
           value={content}
           onChange={(e) => setContent(e.target.value)}
-          className="text-black"
+          className="w-full py-2 px-3 rounded-lg border text-black border-gray-300 focus:outline-none focus:border-indigo-500"
           required
         />
-        <button type="submit">Post</button>
-      </form>
-    </div>
+      </div>
+      <button type="submit" className="bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-700 transition duration-300">Post</button>
+    </form>
+  </div>
+  
   );
 };
