82c3d22aa52ce99db6ce9fc552460eab8d4aec76
Veekthor01 <veekthor01@gmail.com>
2023-11-02 23:34:59 +0300

    update footer

--- chronicles/src/app/Footer/page.js
+++ chronicles/src/app/Footer/page.js
@@ -3,7 +3,7 @@ export default function Footer () {
         <footer className="bg-gray-800 py-4 mt-7">
             <div className="container mx-auto px-4">
                 <p className="text-center text-gray-400 text-sm mb-3">
-                    &copy; 2023 Shortly. All rights reserved.
+                    &copy; 2023 Chronicles. All rights reserved.
                 </p>
                 <p className="text-center text-gray-400 text-sm">
                     Created by Veekthor
