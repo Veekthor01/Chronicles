96db0bc56b17fbc256f0d55c5d7f81391a2bb829
Veekthor01 <veekthor01@gmail.com>
2023-11-04 15:48:32 +0300

    update user page

--- chronicles/src/app/User/page.js
+++ chronicles/src/app/User/page.js
@@ -2,21 +2,23 @@ import Image from 'next/image'
 import Link from 'next/link'
 import BlogPosts from '../BlogPost/page'
 import ProfileDropdown from '../Profile/page'
+import SearchBar from '../../../Components/searchbar'
 
 export default function UserPage() {
   return (
     <main>
-        <nav className="flex flex-row items-center justify-between p-5 bg-green-500">
-      <Image
+        <nav className="flex flex-row items-center justify-between p-3 bg-green-500">
+        <Image
         src="/logo.svg"
         alt='Chronicles Logo'
         width={100}
-        height={25}
-        className=''
+        height={100}
+        className='ml-16 w-40'
         priority
-        />   
-        <div className="flex flex-row justify-end space-x-4">
-        <Link href="/write">Write</Link>
+        /> 
+        <SearchBar />  
+        <div className="flex flex-row justify-end space-x-8 bg-slate-900 p-4 mr-16">
+        <Link href="/write" className='text-sm'>Write</Link>
         <ProfileDropdown />
         </div>
         </nav>
