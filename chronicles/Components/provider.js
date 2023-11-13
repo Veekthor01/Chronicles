21ed8f8f91b472fe20ab797958a0a483b0a3c797
Veekthor01 <veekthor01@gmail.com>
2023-11-13 19:57:30 +0300

    add theme provider

---
+++ chronicles/Components/provider.js
@@ -0,0 +1,19 @@
+'use client'
+import { useState, useEffect } from 'react';
+import { ThemeProvider } from 'next-themes'
+
+export default function Provider({ children }) {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true)
+      }, [])
+
+    if (!mounted) {
+     return <>{children}</>
+     }
+
+    return (
+        <ThemeProvider attribute='class'>{children}</ThemeProvider>
+    )
+}
