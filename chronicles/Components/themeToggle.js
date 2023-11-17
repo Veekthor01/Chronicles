5ece0ba532aced8f6d5e24a4384ff20ab3de3063
Veekthor01 <veekthor01@gmail.com>
2023-11-17 23:54:09 +0300

    add themes toggling

--- chronicles/Components/themeToggle.js
+++ chronicles/Components/themeToggle.js
@@ -13,13 +13,22 @@ export default function ThemeToggle () {
       if (!mounted) {
         return null
       }
+      const themes = ['system', 'light', 'dark'];
 
-    return (
-        <div>
-            The current theme is: {theme}
-            <button onClick={() => setTheme("light")}>Light Mode</button>
-            <button onClick={() => setTheme("dark")}>Dark Mode</button>
-            <button onClick={() => setTheme("system")}>System Mode</button>
+      return (
+        <div className="flex justify-end">
+          <select
+            className="p-2 border border-gray-300 rounded-md bg-gray-800 text-gray-400 focus:outline-none focus:border-indigo-500"
+            onChange={(e) => setTheme(e.target.value)}
+            defaultValue={theme}
+          >
+            {themes.map((theme) => (
+              <option key={theme} value={theme} className="bg-gray-800 text-gray-400 mb-2">
+                {theme}
+              </option>
+            ))}
+          </select>
         </div>
-    )
+      );
+      
 };
