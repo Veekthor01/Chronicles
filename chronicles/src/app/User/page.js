ee37d2f0d6eb39502e901cc609f9aabca13d6ce0
Veekthor01 <veekthor01@gmail.com>
2023-11-17 23:52:20 +0300

    modify like component

--- chronicles/src/app/User/page.js
+++ chronicles/src/app/User/page.js
@@ -1,31 +1,15 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import AuthNavbar from '../../../Components/authNavbar'
 import BlogPosts from '../BlogPost/page'
-import ProfileDropdown from '../Profile/page'
-import SearchBar from '../../../Components/searchbar'
+import Footer from '../../../Components/Footer'
 
 export default function UserPage() {
   return (
-    <main>
-        <nav className="flex flex-row items-center justify-between p-3 bg-green-500">
-        <Image
-        src="/logo.svg"
-        alt='Chronicles Logo'
-        width={100}
-        height={100}
-        className='ml-16 w-40'
-        priority
-        /> 
-        <SearchBar />  
-        <div className="flex flex-row justify-end space-x-8 bg-slate-900 p-4 mr-16">
-        <Link href="/write" className='text-sm'>Write</Link>
-        <ProfileDropdown />
-        </div>
-        </nav>
-
+    <main className='min-h-screen'>
+      <AuthNavbar />
         <div className="">
           <BlogPosts />
         </div>
+        <Footer />
     </main>
   )
 };
