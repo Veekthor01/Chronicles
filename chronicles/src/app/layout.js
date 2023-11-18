54b2205d16d6eb515dd7a995609632398f40bb6b
Veekthor01 <veekthor01@gmail.com>
2023-11-18 23:56:25 +0300

    edit layout

--- chronicles/src/app/layout.js
+++ chronicles/src/app/layout.js
@@ -1,5 +1,9 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+// import font awesome icons to layout page
+import "@fortawesome/fontawesome-svg-core";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>  
+          {children}
+        </body>
     </html>
   )
 }
