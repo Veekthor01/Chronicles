6abffc11bad1fe0b83b58dfe8101a5cab9e91d42
Veekthor01 <veekthor01@gmail.com>
2023-11-18 23:57:02 +0300

    edit logout route

--- Server/Routes/logout.js
+++ Server/Routes/logout.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// logout user and clear session cookie
 router.get('/', (req, res) => {
-    req.logout();
-    res.status(200).json({ message: 'Logout successful' });
-  } 
-);
+  req.logout(); // Passport.js logout
+  res.clearCookie('connect.sid'); // Clear the session cookie
+  req.session.destroy(); // Destroy the session
+  res.status(200).json({ message: 'Logout successful' });
+});
 
 module.exports = router;
