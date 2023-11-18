d1e2671401396cbd2dadb75d207aaf94874ba6af
Veekthor01 <veekthor01@gmail.com>
2023-11-18 23:55:41 +0300

    create auth file

---
+++ Server/Routes/auth.js
@@ -0,0 +1,15 @@
+// routes/auth.js
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  // Check if the user is authenticated based on your session implementation
+  if (req.isAuthenticated()) {
+    res.json({ isAuthenticated: true });
+  } else {
+    res.json({ isAuthenticated: false });
+  }
+});
+
+module.exports = router;
