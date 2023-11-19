afac2f170dbb0cd191c7264a02856cd2e2da34ad
Veekthor01 <veekthor01@gmail.com>
2023-11-19 23:50:36 +0300

    update github route

--- chronicles/src/app/Login/Github-Login/page.js
+++ chronicles/src/app/Login/Github-Login/page.js
@@ -1,5 +1,7 @@
 'use client'
 import { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function GitHubLogin() {
   const [isLoading, setIsLoading] = useState(false);
@@ -20,7 +22,8 @@ export default function GitHubLogin() {
 
   return (
     <div>
-      <button onClick={handleGitHubLogin} disabled={isLoading} className="bg-indigo-500 text-white py-2 px-4 rounded-lg w-full">
+      <button onClick={handleGitHubLogin} disabled={isLoading} className="border border-gray-500 hover:bg-gray-300 dark:hover:bg-gray-700 text-sm tracking-wide text-gray-900 dark:text-gray-200 py-2 px-4 rounded-lg w-full">
+      <FontAwesomeIcon icon={faGithub} className="mr-2 text-base" />
         {isLoading ? 'Logging in...' : 'Log in with GitHub'}
       </button>
     </div>
