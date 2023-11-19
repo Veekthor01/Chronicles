39405e7984221ea17c5f3916ad0033223df57fa9
Veekthor01 <veekthor01@gmail.com>
2023-11-19 23:49:49 +0300

    forgot password page

--- chronicles/src/app/ForgotPassword/page.js
+++ chronicles/src/app/ForgotPassword/page.js
@@ -16,18 +16,24 @@ function ForgotPassword() {
     };
 
     return (
-        <div>
-            <h2>Forgot Password</h2>
-            <h3>Enter your email address to reset your password.</h3>
-            <label className='text-gray-600'>Email:</label>
+        <div className='min-h-screen flex items-center justify-center'>
+            <div className="flex flex-col items-center">
+            <div className="max-w-md w-full px-8 py-10 bg-white dark:bg-slate-800 rounded-md shadow-xl">
+            <h2 className='mt-2 text-center text-2xl font-bold text-gray-900 dark:text-gray-200 mb-4 tracking-wide'>Forgot Password</h2>
+            <h3 className='text-xl mb-4 text-center font-bold text-gray-900 dark:text-gray-200 tracking-wide'>Enter your registered email address to reset your password.</h3>
+            <label className='inline-block mb-1 text-sm font-medium text-gray-900 dark:text-gray-200 tracking-wide'>Email</label>
             <input
                 type="email"
                 placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                className="w-full mb-2 bg-white dark:bg-gray-700 py-2 px-2 rounded-lg border text-gray-900 dark:text-gray-200 border-gray-300 focus:outline-none focus:border-indigo-700"
+                required
             />
-            <button onClick={handleForgotPassword}>Send Reset Link</button>
-            <p>{message}</p>
+            <button onClick={handleForgotPassword} className='mt-4 bg-indigo-600 text-sm text-white py-2 px-4 rounded-lg hover:bg-indigo-700 transition duration-300 w-full tracking-wide'>Send Reset Link</button>
+            <p className="text-green-500 mt-2 text-center text-sm tracking-wide">{message}</p>
+        </div>
+        </div>
         </div>
     );
 }
