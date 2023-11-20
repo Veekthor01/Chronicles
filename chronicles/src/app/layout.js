3f775d990783dea787f81a1b60c654771497f98c
Veekthor01 <veekthor01@gmail.com>
2023-11-21 00:26:07 +0300

    Update body class in RootLayout component

--- chronicles/src/app/layout.js
+++ chronicles/src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>  
+      <body className={`${inter.className} bg-gray-100 dark:bg-slate-900`}> 
           {children}
         </body>
     </html>
