e85881c622076cf774fc85e776ccbcd1314e4afc
Veekthor01 <veekthor01@gmail.com>
2023-11-21 23:13:31 +0300

    layout for change password page

---
+++ chronicles/src/app/Profile/Change-Password/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'Change Password',
+    description: 'Change your password',
+  }
+  
+export default function ChangePasswordLayout({ children }) {
+    return (
+    <section>{children}</section>
+    )
+  }
