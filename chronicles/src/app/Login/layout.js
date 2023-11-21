977c242b32c7395e27f12109ea2b3db9d428834b
Veekthor01 <veekthor01@gmail.com>
2023-11-21 23:12:29 +0300

    layout for login page

---
+++ chronicles/src/app/Login/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'Sign in ',
+    description: 'Sign in to your account',
+  }
+  
+export default function LoginLayout({ children }) {
+    return (
+    <section>{children}</section>
+    )
+  }
