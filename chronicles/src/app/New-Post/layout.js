cc01aed1163963b1d57c9f21131627b91b8b3e53
Veekthor01 <veekthor01@gmail.com>
2023-11-21 23:12:56 +0300

    layout for post

---
+++ chronicles/src/app/New-Post/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'Create New Post',
+    description: 'Create a new post',
+  }
+  
+export default function NewPostLayout({ children }) {
+    return (
+    <section>{children}</section>
+    )
+  }
