768ad2fb8b83cd7b82b93516e998463b081fca91
Veekthor01 <veekthor01@gmail.com>
2023-11-21 23:11:41 +0300

    layout file for password page

---
+++ chronicles/src/app/ForgotPassword/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'Forgot Password',
+    description: 'Enter your email to reset your password',
+  }
+  
+export default function ForgotPasswordLayout({ children }) {
+    return (
+    <section>{children}</section>
+        )
+  }
