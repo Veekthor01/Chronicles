fc24922ae567bef2d89f180b53fb49efc960514d
Veekthor01 <veekthor01@gmail.com>
2023-11-22 23:25:07 +0300

    Refactor deleteUser route to use session user ID

--- Server/Routes/deleteUser.js
+++ Server/Routes/deleteUser.js
@@ -4,14 +4,16 @@ const { deleteUser } = require('../DB/user');
 const isAuthenticated = require('../Passport-Config/Authenticated');
 
 // DELETE user by ID
-router.delete('/:id', isAuthenticated, async (req, res) => {
-  const userId = req.params.id;
+router.delete('/', isAuthenticated, async (req, res) => {
+  const userId = req.user._id; // Get the user ID from the session
   try {
     const deletedUser = await deleteUser(userId);
     if (!deletedUser) {
       // User not found
       return res.status(404).json({ message: 'User not found' });
     }
+    // Clear the session after deleting the user (optional)
+    req.session.user = null;
     return res.status(200).json({ message: 'User deleted successfully' });
   } catch (error) {
     return res.status(500).json({ message: 'Failed to delete user', error: error.message });
