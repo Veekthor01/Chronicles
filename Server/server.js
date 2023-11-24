313cd3e208ec9859e4420d45dfabca81537b2979
Veekthor01 <veekthor01@gmail.com>
2023-11-24 19:02:57 +0300

    Commented out FrontendURL in server.js

--- Server/server.js
+++ Server/server.js
@@ -26,7 +26,7 @@ const authRouter = require('./Routes/auth');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const FrontendURL = process.env.FRONTEND_URL;
+//const FrontendURL = process.env.FRONTEND_URL;
 const secretKey = process.env.SECRET_KEY;
 const mongoURI = process.env.MONGODB_URI;
 const MongoDBStore = connectMongoDBSession(session);
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 const corsOptions = {
-    origin: FrontendURL,
+    //origin: FrontendURL,
     credentials: true,
 };
 app.use(cors(corsOptions));
