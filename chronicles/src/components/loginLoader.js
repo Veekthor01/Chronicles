7c269875e9112ed46a4406b243ce2c2bbade3b59
Veekthor01 <veekthor01@gmail.com>
2023-11-30 23:43:53 +0300

    Add LoadingOverlay component

---
+++ chronicles/src/components/loginLoader.js
@@ -0,0 +1,10 @@
+// LoadingOverlay.js
+export default function LoadingOverlay({ message }) {
+    return (
+      <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-slate-900 bg-opacity-50 text-white text-2xl">
+        <div className="text-center">
+          <h2 className="text-lg md:text-2xl font-bold mb-4 text-gray-900 dark:text-gray-200">{message}</h2>
+        </div>
+      </div>
+    );
+  }
